6d15e4917eaba5e1759b57743eb5644839cdcc99
Zafer BAHADIR <zbahadir@msn.com>
2022-05-04 17:45:04 +0300

    added view

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // ignore: non_constant_identifier_names
-  List list_name = ["Tab1", "Tab2", "Tab3", "Tab4", "New Tab"];
+  List list_name = ["Tab1", "Tab2", "Tab3", "New Tab"];
 
   @override
   Widget build(BuildContext context) {
